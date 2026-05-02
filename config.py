13e261af92ce74535a5bedf6355df2a7986c117f
shodiyevartur1-pixel <shodiyevartur1@gmail.com>
2026-05-02 13:38:00 +0500

    First commit

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
 
-# Bot Tokeni (Telegram @BotFather dan olingan)
-BOT_TOKEN = "8698487561:AAE4lyN_H50tckwj7fZTKNcMO1JOGeM60Ig"
+# Bot Tokeni - Avval Environment Variable dan oladi, yo'qsa pastdagisini ishlatadi
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8698487561:AAE4lyN_H50tckwj7fZTKNcMO1JOGeM60Ig")
 
-# Admin ID raqamlari (RO'YXAT ko'rinishida)
-ADMIN_ID = [8059999086, 7612085208]
+# Admin ID lar (string ko'rinishida kelganda listga aylantiramiz)
+admin_ids_str = os.environ.get("ADMIN_ID", "8059999086,7612085208")
+ADMIN_ID = [int(id.strip()) for id in admin_ids_str.split(',') if id.strip()]
 
 # Anti-spam sozlamalari
-SPAM_LIMIT = 100000          # Nechta xabar
-SPAM_WINDOW = 100000         # Qancha vaqt ichida (soniyada)
+SPAM_LIMIT = 100000
+SPAM_WINDOW = 100000
 
 # Ma'lumotlar bazasi fayli
 DB_NAME = "bot_database.db"
